5e7770e447098f97d1a51b4a1c19491c2cdf4309
Casey Scarborough <caseyscarborough@gmail.com>
2013-04-11 23:07:15 -0400

    Added rename and put functionality

--- ftp.rb
+++ ftp.rb
@@ -1,17 +1,28 @@
 #!/Users/Casey/.rvm/rubies/ruby-2.0.0-p0/bin/ruby
  
 require 'net/ftp'
-require 'highline/import'
+begin
+	require 'highline/import'
+rescue
+	puts "Rubygem highline not found. Password will be shown in plain text."
+end
 
 def display_help
-	help = 
-	"Usage: ruby ftp.rb [-?|-h|[user@host]]\n
+	abort "Usage: ruby ftp.rb [-?|-h|[user@host]]
 Parameters:
      -?           displays the usage information
      -h           displays the usage information
      user@host    attempts to make a connection to the specified host
-                  using the username provided"
-  	abort help
+                  using the username provided
+Once connected the user has the following options:
+     ls                   displays a list of the current directory's contents
+     pwd                  displays the path to the current working directory
+     cd [directory]       changes to the specified directory
+     get [file]           downloads the specified file
+     put [file]           uploads the specified file from the local system
+     rm [file]            deletes the specified file
+     rename [file] [file] renames the first file to the name of the second file
+     exit                 close the ftp connection and exit the application"
 end
 
 def open_connection(server_name, username, password)
@@ -35,16 +46,30 @@ def get_file(dir)
 	end
 end
 
+def put_file(file)
+	if(File.exist?(file))
+		begin
+			puts "Uploading file..."
+			$ftp.put(file)
+			puts "File successfully uploaded!"
+		rescue
+			puts "There was an error trying to upload the file."
+		end
+	else
+		puts "The specified file does not exist on the local system."
+	end
+end
+
 def change_directory(dir)
-	# begin
+	begin
 		if(dir.end_with?("/"))
 			$ftp.chdir(dir)
 		else
 			$ftp.chdir(dir + "/")
 		end
-	# rescue
-	# 	puts "Directory does not exist!"
-	# end
+	rescue
+		puts "Directory does not exist!"
+	end
 end
 
 def delete_file(file_to_delete)
@@ -68,6 +93,21 @@ def delete_file(file_to_delete)
 	end
 end
 
+def rename_file(file1, file2)
+	list = $ftp.list
+	list.each do |file|
+		if (file.end_with?(file1))
+			begin
+				$ftp.rename(file1, file2)
+				prompt
+			rescue
+				puts "Unable to rename file."
+			end
+		end
+	end
+	puts "File #{file1} not found."
+end
+
 def prompt
 	user_input = ""
 	until (user_input == "exit")
@@ -84,12 +124,20 @@ def prompt
 		elsif (user_input.start_with?("get "))
 			file = user_input.gsub(/get /, '')
 			get_file(file)
+		elsif (user_input.start_with?("put "))
+			file = user_input.gsub(/put /, '')
+			put_file(file)
 		elsif (user_input.start_with?("rm "))
 			file = user_input.gsub(/rm /, '')
 			puts file
 			delete_file(file)
 		elsif (user_input == "pwd")
 			puts $ftp.pwd
+		elsif (user_input.start_with?("rename "))
+			files = user_input.gsub(/rename /, '')
+			file1 = files.gsub(/\s[A-Za-z0-9%&#$@!*()_.]{0,255}/, '')
+			file2 = files.gsub(/[A-Za-z0-9%&#$@!*()_.]{0,255}\s/, '')
+			rename_file(file1, file2)
 		elsif (user_input == "exit")
 			puts "Closing connection... Goodbye!"
 			abort $ftp.close
@@ -105,11 +153,19 @@ elsif (/[A-Za-z0-9]@[A-Za-z0-9]/.match(ARGV[0]))
 	username_hostname = ARGV[0]
 	username = username_hostname.gsub(/@[A-Za-z0-9]{0,255}.[A-Za-z0-9]{2,4}/, '')
 	hostname = username_hostname.gsub(/[A-Za-z0-9]{0,255}@/, '')
-	password = ask("Enter password: ") { |p| p.echo = false }
+	begin
+		password = ask("#{username_hostname}\'s password: ") { |p| p.echo = false }
+	rescue
+		print "#{username_hostname}\'s password: "
+		password = $stdin.gets.strip
+	end
 	$ftp = open_connection(hostname, username, password)
 	if($ftp)
 		prompt
 	end
+else
+	puts "ftp.rb: illegal option -- #{ARGV[0]}"
+	display_help
 end
 
  
